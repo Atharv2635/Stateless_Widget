8259cfa2b55478cf342473e5395b24543bf136cf
Atharv Dabhade <atharvdabhade101@gmail.com>
2024-10-09 08:54:18 +0530

    Initialized

---
+++ Column_Combinations/Combination_2/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "First Combination",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+            ),
+            ),
+            backgroundColor: Colors.cyan,
+            centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [ 
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
