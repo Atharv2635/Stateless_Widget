a3e6d2507801764a0d28e5b6642064f0a8a19a8e
Atharv Dabhade <atharvdabhade101@gmail.com>
2024-10-09 09:30:26 +0530

    Initialized

---
+++ Space_Parameters/Space_Between/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Space Between",
+          style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+        ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepOrange,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple,
+            )
+          ],
+        ),
+      ),
+      ),
+    );
+  }
+}
